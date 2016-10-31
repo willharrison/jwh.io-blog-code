d161fddc32a2cf5ed2fbb55df74a4f4ef6edf3ef
Will Harrison <jwh335@gmail.com>
2016-10-30 22:18:27 -0400

    add shell project

---
+++ Shell/ListDirectories/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListDirectories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var files = Directory.GetFiles(Directory.GetCurrentDirectory());
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+    }
+}
---
+++ Shell/Shell/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shell = new Shell();
+            shell.Run();
+        }
+    }
+
+    public class Shell
+    {
+        private Dictionary<string, string> Aliases = new Dictionary<string, string>
+        {
+            { "ls", @".\ListDirectories.exe" },
+            { "clear", @".\Clear.exe" }
+        };
+
+        public void Run()
+        {
+            string input = null;
+
+            do
+            {
+                Console.Write("$ ");
+                input = Console.ReadLine();
+                Execute(input);
+            } while (input != "exit");
+        }
+
+        public int Execute(string input)
+        {
+            if (Aliases.Keys.Contains(input))
+            {
+                var process = new Process();
+                process.StartInfo = new ProcessStartInfo(Aliases[input])
+                {
+                    UseShellExecute = false
+                };
+
+                process.Start();
+                process.WaitForExit();
+
+                return 0;
+            }
+
+            Console.WriteLine($"{input} not found");
+            return 1;
+        }
+    }
+}
